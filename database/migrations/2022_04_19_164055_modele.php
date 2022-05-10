794a9fdf65b5c4fa0751e84cc60ff26c6dca9446
Rafcik177 <rafcik1771@gmail.com>
2022-05-10 22:46:53 +0200

    Update 2022_04_19_164055_modele.php

--- database/migrations/2022_04_19_164055_modele.php
+++ database/migrations/2022_04_19_164055_modele.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -16,13 +16,35 @@ return new class extends Migration
         Schema::create('modele', function (Blueprint $table) {
             $table->id();
             $table->string('nazwa');
-            $table->string('kategoria');
+            $table->enum('kategoria', ['Lokomotywa', 'Wagon pasażerski', 'Wagon towarowy']);
             $table->integer('moc');
             $table->integer('ilosc_miejsc')->NULL;
             $table->integer('max_predkosc')->NULL;
             $table->integer('waga');
-            $table->integer('cena');
+            $table->string('cena', 20);
         });
+        DB::table('modele')->insert(
+            array(
+                'nazwa' => '35WE',
+                'kategoria' => 'Lokomotywa',
+                'moc' => 400,
+                'ilosc_miejsc' => 4,
+                'max_predkosc' => 160,
+                'waga' => 130,
+                'cena' => 25980000,
+            )
+            );
+            DB::table('modele')->insert(
+            array(
+                'nazwa' => '45WE',
+                'kategoria' => 'Lokomotywa',
+                'moc' => 480,
+                'ilosc_miejsc' => 4,
+                'max_predkosc' => 190,
+                'waga' => 160,
+                'cena' => 31580000,
+                 )
+            );
     }
 
     /**
