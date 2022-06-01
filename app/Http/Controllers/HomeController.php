67d327d5eedeee3e50c4b1aaaa2198fc9f6a6e6d
Rafcik177 <rafcik1771@gmail.com>
2022-06-01 09:57:32 +0200

    Usunięcie niepotrzebnego

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,40 +25,4 @@ class HomeController extends Controller
     {
         return view('home');
     }
-    public function szukajpracownika()
-    {
-        return view('/pracownicy/szukajpracownika');
-    }
-
-
-    public function pracownik(Request $request)
-    {
-        
-
-        $id= $request->id_pracownika;
-            
-            $users_count = DB::table('pracownicy')
-            ->where('id', '=', $id)
-            ->count();
-            if($users_count>0)
-            {
-                $user = DB::table('pracownicy')->where('id', $id)->first();
-                $imie=$user->imie;
-                $nazwisko=$user->nazwisko;
-                $data_urodzenia=$user->data_urodzenia;
-                $stanowisko=$user->stanowisko;
-                $czy_kierownik=$user->czy_kierownik;
-                $id_wydzialu=$user->id_wydzialu;
-                $ready=1;
-                return view('pracownicy/pracownik',['ready' => $ready, 'imie'=>$imie, 'nazwisko'=>$nazwisko,'data_urodzenia'=>$data_urodzenia,'stanowisko'=>$stanowisko,'id_wydzialu'=>$id_wydzialu,'czy_kierownik'=>$czy_kierownik]);
-            }
-            else
-            {
-                $ready=0;
-                $error="Nie ma takiego użytkownika";
-                return view('pracownicy/pracownik',['error' => $error,'ready' => $ready]);
-            }
-
-       
-    }
 }
